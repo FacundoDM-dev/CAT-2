024f8081196efdd4b2fd3ec12cf3e2d7e4383005
FacundoDM-dev <delmedicofacundomartin@gmail.com>
2023-10-31 13:33:24 +0100

    PostIt & PostItList Stable

--- postIt/src/components/PostItList.vue
+++ postIt/src/components/PostItList.vue
@@ -1,7 +1,14 @@
-<script>
-import {PostIt} from '../components/posTit.vue'
+<template>
+  <div class="postit-list">
+    <PosTit v-for="postIt in postItArray" :key="postIt.id" :postIt="postIt" />
+  </div>
+</template>
 
-const postItArray = [
+<script setup>
+import {ref} from "vue"
+import PosTit from './PosTit.vue';
+
+const postItArray = ref([
   {
     id: 1,
     title: "Important Meeting",
@@ -9,7 +16,6 @@ const postItArray = [
     date: "2023-10-30",
     time: "09:00 AM",
     dueDate: "2023-11-15",
-    dueTime: "10:00 AM",
     priority: "high",
     content: "Prepare for the quarterly review meeting with the team.",
     labels: ["Work", "Meeting", "Urgent"],
@@ -22,7 +28,6 @@ const postItArray = [
     date: "2023-10-30",
     time: "10:00 AM",
     dueDate: "2023-10-31",
-    dueTime: "12:00 PM",
     priority: "medium",
     content: "Buy groceries for the week: milk, eggs, bread, and vegetables.",
     labels: ["Personal", "Groceries"],
@@ -35,23 +40,20 @@ const postItArray = [
     date: "2023-10-31",
     time: "02:00 PM",
     dueDate: "2023-11-10",
-    dueTime: "04:00 PM",
     priority: "high",
     content: "Finish the project report and submit it before the deadline.",
     labels: ["Work", "Project", "Urgent"],
     featured: false,
   },
-  // Add more post-its as needed
-];
+  // Agrega más tarjetas Post-It según sea necesario
+]);
 </script>
 
-<template>
-  <div>
-    <!-- Utiliza v-for para representar varios posTit -->
-    <PosTit v-for="postIt in postItArray" :key="postIt.id" :postIt="postIt" />
-  </div>
-</template>
-
 <style scoped>
+.postit-list {
+display: grid;
+width: 100%;
+grid-template-columns: 1fr 1fr 1fr;
+}
 
 </style>
